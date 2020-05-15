8e9ded91175325ac557466d09df920ac70b6ec23
Diptanshu8 <diptanshuj@gmail.com>
2020-05-15 14:53:49 +0530

    Add support to showcase IP address on the home page on user click. Split the block dependent code to functions.html. Added a new route to showcase the IP.
    Signed-off-by: Diptanshu8 <diptanshuj@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,24 @@
 from app import app
+from flask import render_template
+# Userdetails for base.html
+user = {'username':'Taansh'}
+
+# Feature list to enumerate all the features being provided right now
+features = [
+    {
+	    'name' : 'IP Address',
+	    'Desc' : "Display the IP address of the raspberry pi"
+    },
+]
+@app.route('/get_ip_addr')
+def get_ip_addr():
+    import subprocess
+    ip_addr = None
+    o = subprocess.run(["hostname","-I"], capture_output=True)
+    if (o.returncode==0):
+    	ip_addr = o.stdout
+    return render_template('functions.html', title="Diptanshu's", user = user, ip_addr = ip_addr)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('functions.html', title="Diptanshu's", user = user, features = features)
