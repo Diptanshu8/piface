95c611fc75043ae704897f9780adf05043ab1069
Diptanshu8 <diptanshuj@gmail.com>
2020-05-15 16:29:13 +0530

    Added support for NAS MOUNT status and deluge status. NAS_MOUNT_STATUS automatically mounts. Failure must be some issue. Work in progress for some buttons and beautification.
    Signed-off-by: Diptanshu8 <diptanshuj@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
 from app import app
 from flask import render_template
+
+# Constants
+NAS_MNT_POINT = "/home/pi/Taansh_HD"
+NAS_MNT_CMD = ["sudo","mount.cifs","-vvv","//192.168.1.1/DJ","/home/pi/Taansh_HD/", "-o", "guest,vers=1.0"]
+
 # Userdetails for base.html
 user = {'username':'Taansh'}
 
@@ -9,6 +14,10 @@ features = [
 	    'name' : 'IP Address',
 	    'Desc' : "Display the IP address of the raspberry pi"
     },
+    {
+            'name' : "NAS mount",
+            'Desc' : "Displays status of NAS mount"
+         },
 ]
 @app.route('/get_ip_addr')
 def get_ip_addr():
@@ -18,6 +27,27 @@ def get_ip_addr():
     if (o.returncode==0):
     	ip_addr = o.stdout
     return render_template('functions.html', title="Diptanshu's", user = user, ip_addr = ip_addr)
+
+@app.route('/nas_mount_status')
+def nas_mount_status():
+    import os,subprocess
+    mount_status = os.path.ismount(NAS_MNT_POINT)
+    if not mount_status:
+        subprocess.run(NAS_MNT_CMD, capture_output=True) 
+        mount_status = os.path.ismount(NAS_MNT_POINT)
+
+    return render_template('functions.html', title="Diptanshu's", user = user, mount_status = os.path.ismount(NAS_MNT_POINT))
+    
+@app.route('/deluge_status')
+def deluge_status():
+    import psutil as ps
+    d_status = "Disabled"
+    pidlist = [(p.pid, p.name()) for p in ps.process_iter()]
+    for p in pidlist:
+        if "deluge" in p[1]:
+            d_status = "Enabled"
+    print("Inside deluge_status for d_status = "+ d_status)
+    return render_template('functions.html', title="Diptanshu's", user = user, d_status = d_status)
 
 @app.route('/')
 def index():
--- piface.py
+++ piface.py
@@ -1,3 +1,5 @@
 from app import app
 if __name__ == '__main__':
+    app.config(config.Config)
+    print(app.config)
     app.run(debug=True, host='0.0.0.0')
