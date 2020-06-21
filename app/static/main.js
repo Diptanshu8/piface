b8a261f0ff3543b04ed19a117901a3a3334812f4
Diptanshu8 <diptanshuj@gmail.com>
2020-06-21 13:27:01 +0530

    Moved homepage button to navbar updated to update button colour for server response code. updated JS to not take button id but retrieve it from event.target.id Signed-off-by: Diptanshu8 <diptanshuj@gmail.com>

--- app/static/main.js
+++ app/static/main.js
@@ -10,7 +10,6 @@
         // Value = "Text appearing on page", "corresponding callback to server to process the request",
         //          "status of the ng-disabled variable", "output to be displayed"
     $scope.features = {};
-    $scope.features['home_pg_btn'] = ['Home Page','/', false, ''];
     $scope.features['ip_addr_btn'] = ['IP Address','/get_ip_addr', false, ''];
     $scope.features['mnt_stat_btn'] = ['NAS mount status','/nas_mount_status', false, ''];
     $scope.features['delg_stat_btn'] = ['Deluge Daemon status','/deluge_status', false, ''];
@@ -20,16 +19,12 @@
     $scope.features['reboot_btn'] = ['Reboot Raspi','/reboot', false, ''];
 	$scope.present = [];
     
-        $scope.btn_click = function(btn_id) {
+        $scope.btn_click = function(event) {
             var i;
             var found = false;
-            $log.log(btn_id);
+            var btn_id = event.target.id;
+            var button = angular.element(document.getElementById(event.target.id));
 
-            // Clear the present variable when homepage button is clicked.
-            if (btn_id == 'home_pg_btn') {
-                $scope.present = [];
-                return;
-            }
             // Check if the feature is being already displayed and raise an 
             // alert in that case.
             // Else find the feature to be displayed from btn_id, process
@@ -54,10 +49,14 @@
                           $scope.present[j][3]=data;
                           $scope.present[j][2] = true;
                           $log.log(data);
+                          button.removeClass("btn-primary");
+                          button.addClass("btn-success");
                         }).
                         error(function(error) {
                             $scope.present[j][3]=error;
                             $scope.present[j][2] = true;
+                            button.removeClass("btn-primary");
+                            button.addClass("btn-danger");
                             $log.log(error);
                         });
                 }
