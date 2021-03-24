341894db01dba1c9e75c0a61001f81651fa48a7e
Diptanshu8 <diptanshuj@gmail.com>
2021-03-24 15:51:08 +0530

    Add timeout for button clicks to enable multiple clicks on same button.
    Also handle corner cases of button ID mismatch in button_click.
    
    Signed-off-by: Diptanshu8 <diptanshuj@gmail.com>

--- app/static/main.js
+++ app/static/main.js
@@ -2,8 +2,8 @@
 
   angular.module('PiFaceApp', [])
 
-  .controller('PiFaceController', ['$scope', '$log', '$http',
-    function($scope, $log, $http) {
+  .controller('PiFaceController', ['$scope', '$log', '$http', '$timeout',
+    function($scope, $log, $http, $timeout) {
     $scope.buttons_list = [
         {
             'id'      : 'IP Address',
@@ -59,38 +59,45 @@
         var button = angular.element(document.getElementById(src_id));
         // Check if the ID from ng-click matches the IDs present in the list of buttons.
         // Either of too many buttons with same ID or no buttons with matching ID is a problem.
-        // TODO: Handle the errors in seperate alerts.
+        // buttons is a list of all the buttons in buttons_list which match the ID of event source button.
         var buttons = $scope.buttons_list.filter(function(obj) {
                     return obj.id == src_id;});
-        if (buttons.length != 1)
+        if (buttons.length == 0)
             alert(src_id + " is invalid button ID.");
+        else if (buttons.length > 1) {
+            $log.log(buttons);
+            alert("Multiple buttons have same ID as the event source button. Please check the console log.");
+        }
         else {
+            // Ideally buttons should contain just one element matching the ID of event source button.
             $log.log(src_id);
             var b = buttons[0];
-            if (button.hasClass("btn-primary")) {
-                var old = button.text() + ": ";
-                button.removeClass("btn-primary");
-                // TODO: add timeout and remove just one time click feature via btn-primary class check.
-                $http.get(b.route).
-                    success(function(data) {
-                        $log.log(data);
-                        button.text(old+data);
-                        button.addClass("btn-success");
-                        // Redirects to a new tab with respective page.
-                        if (b.redirect!="") {
-                            window.open("//" + window.location.hostname + ":" +b.redirect);
-                        }
-                    }).
-                    error(function(error) {
-                        $log.log(error);
-                        button.text(old+error);
-                        button.addClass("btn-danger");
-                    });
-            }
-            else 
-                alert("Status already being shown. Reload and click again!");
+            var old = button.text();
+            button.removeClass("btn-primary");
+            $http.get(b.route).
+                success(function(data) {
+                    $log.log(data);
+                    button.text(old+":"+data);
+                    button.addClass("btn-success");
+                    // Redirects to a new tab with respective page.
+                    if (b.redirect!="")
+                        window.open("//" + window.location.hostname + ":" +b.redirect);
+                }).
+                error(function(error) {
+                    $log.log(error);
+                    button.text(old+error);
+                    button.addClass("btn-danger");
+                });
+                $timeout(function(){
+                    button.text(old);
+                    if (button.hasClass("btn-success"))
+                        button.removeClass("btn-success");
+                    if (button.hasClass("btn-danger"))
+                        button.removeClass("btn-danger");
+                    button.addClass("btn-primary");
+                }, 3000);
         }
-        };
+    };
     }
   ]);
 }());
