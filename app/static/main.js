afd45e208f061e06c72adbd0bf27f0a1e887e5fd
Diptanshu8 <diptanshuj@gmail.com>
2021-03-24 18:23:03 +0530

    Major update with side panel for toggling button display on and off.
    Hacky solution with black color for button toggler.
    
    WIP:
    1. Handle multiple clicks on button when the response is yet to be
    received from the server.
    2. Add green and red circles in front of name of button in button
    togler.
    3. Modify the black side panel to same color as top nav.
    
    Signed-off-by: Diptanshu8 <diptanshuj@gmail.com>

--- app/static/main.js
+++ app/static/main.js
@@ -101,3 +101,19 @@
     }
   ]);
 }());
+
+// Function to load the sidebar depending on the device width.
+$(function(){
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $(window).resize(function(e) {
+      if($(window).width()<=768){
+        $("#wrapper").removeClass("toggled");
+      }else{
+        $("#wrapper").addClass("toggled");
+      }
+    });
+});
