ada27f40e5905c06c8d2f2b4d870e3907f4837a9
Diptanshu8 <diptanshuj@gmail.com>
2022-04-23 16:43:32 +0530

    Update the log file name and reduce the size of log files.

--- app/logger.py
+++ app/logger.py
@@ -2,9 +2,9 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
-logFile = "server.log"
+logFile = "piface.log"
 
-handler = RotatingFileHandler(logFile, mode='a', maxBytes=5*1024*1024, backupCount=2, encoding=None, delay=0)
+handler = RotatingFileHandler(logFile, mode='a', maxBytes=5*1024, backupCount=2, encoding=None, delay=0)
 handler.setFormatter(log_formatter)
 handler.setLevel(logging.INFO)
 
